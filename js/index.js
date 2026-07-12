b8a9e304cafbcd569d80b519c47699da65c47a47
kigo121308-debug <kigo121308@gmail.com>
2026-07-12 23:13:27 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const homeNews = document.getElementById("home-news-list");
+
+fetch("news.json")
+    .then(res => res.json())
+    .then(data => {
+
+        // 日付順に並び替え（新しい順）
+        data.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+        // 最新5件だけ取得
+        const latest = data.slice(0, 5);
+
+        latest.forEach(item => {
+
+            const date = item.date.replaceAll("-", ".");
+
+            const target = item.url.startsWith("http")
+                ? 'target="_blank" rel="noopener"'
+                : "";
+
+            homeNews.innerHTML += `
+                <li>
+
+                    <time datetime="${item.date}">
+                        ${date}
+                    </time>
+
+                    <p class="category ${item.category}">
+                        ${item.category.toUpperCase()}
+                    </p>
+
+                    <p>
+                        <a href="${item.url}" ${target}>
+                            ${item.title}
+                        </a>
+                    </p>
+
+                </li>
+            `;
+        });
+
+    })
+    .catch(err => console.error(err));
