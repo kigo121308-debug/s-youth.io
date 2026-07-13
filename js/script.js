c09adbff1adff79f1a593b34c9a2dfcd55b58291
kigo121308-debug <kigo121308@gmail.com>
2026-07-14 02:31:19 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -38,3 +38,34 @@ if (touchDevice) {
   });
 
 }
+
+const targets = document.querySelectorAll(
+  '.fade-up, .fade-left, .fade-right, .zoom-in'
+);
+
+const observer = new IntersectionObserver((entries)=>{
+
+  entries.forEach(entry=>{
+
+    if(entry.isIntersecting){
+
+      entry.target.classList.add('show');
+
+      observer.unobserve(entry.target);
+
+    }
+
+  });
+
+},{
+  threshold:0.15,
+  rootMargin:'0px 0px -10% 0px'
+});
+
+targets.forEach((target,index)=>{
+
+  target.style.transitionDelay = `${index * 0.08}s`;
+
+  observer.observe(target);
+
+});
